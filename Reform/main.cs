5a2c97bfea8258f788f099569cc82f30e65acece
Chen Huang <chinux@gmail.com>
2020-05-03 23:04:06 -0700

    First Commit. Add boilerplate code

---
+++ Reform/main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+namespace Reform
+{
+  public class Main : MBSubModuleBase
+  {
+    protected override void OnSubModuleLoad()
+    {
+      Module.CurrentModule.AddInitialStateOption(new InitialStateOption(
+        "TestReformOption", new TextObject("Click Me", null), 9990, () =>
+        {
+          InformationManager.DisplayMessage(
+            new InformationMessage("HelloWorld"));
+        }, false));
+    }
+  }
+}
