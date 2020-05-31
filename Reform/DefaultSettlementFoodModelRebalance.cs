8836d9de2e3553439e10ad457cd92db73fcff09c
Chen Huang <chinux@gmail.com>
2020-05-31 14:09:40 -0700

    Village provides more food to settlement.
    Raided village will have bigger impact to bounded settlement in terms of food.

---
+++ Reform/DefaultSettlementFoodModelRebalance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+
+namespace Reform
+{
+  [HarmonyPatch(typeof(Village), "HearthLevel")]
+  internal class SettlementFoodStocksChange
+  {
+    public static void Postfix(
+      ref int __result,
+      Village __instance)
+    {
+      __result = (int)__instance.Hearth / 100;
+    }
+  }
+}
